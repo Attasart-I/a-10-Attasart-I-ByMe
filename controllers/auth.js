5bfcc4699afaf551accc26e50bfa36f24033aa84
Attasart Inpun 6833287221 <attasart2548@gmail.com>
2026-02-25 21:18:46 +0700

    Assignment10

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,7 +37,8 @@ exports.register = async (req, res, next) => {
 // @route   POST /api/v1/auth/login
 // @access  Public
 exports.login = async (req, res, next) => {
-
+  try {
+  
   const { email, password } = req.body;
 
   // Validate email & password
@@ -76,7 +77,9 @@ exports.login = async (req, res, next) => {
 //     token
 //   });
     sendTokenResponse(user,200,res);
-
+  } catch (err){
+      return res.status(401).json({success: false, msg: 'Cannot convert email or password to string'});
+  }
 };
 
 // Get token from model, create cookie and send response
@@ -112,3 +115,18 @@ exports.getMe = async (req, res, next) => {
     const user = await User.findById(req.user.id);
     res.status(200).json({success:true, data:user});
 }
+
+// @desc    Log user out / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10*100),
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+};
